b9306f18170bcd04c1aee6142c1982aad0d9463a
charles youngman <charlesyoungman@charless-MacBook-Air.local>
2021-07-04 13:39:35 -0400

    adding new files

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -18,18 +18,29 @@ def scrape():
   soup = BeautifulSoup(response.text, 'html.parser')
   news_title = soup.title.text
 
+  mars_dict["news_title"]=news_title
+
   paragraphs = soup.find_all('p')
   for paragraph in paragraphs:
 #     
     news_p = paragraph.text
   news_p  
 
-  mars_dict["news_title"]=news_title
   mars_dict["news_p"]=news_p
 
+  url = 'https://spaceimages-mars.com/'
+  browser.visit(url) 
   featured_image_url = 'https://spaceimages-mars.com/image/featured/mars3.jpg'
 
   mars_dict["featured_image_url"]=featured_image_url
+
+  mars_url = 'http://space-facts.com/mars/'
+  tables = pd.read_html(mars_url)
+  df = tables[0]
+  df.head(10)
+  html_table = df.to_html()
+  
+  mars_dict["html_table"]=html_table
 
   url = 'https://marshemispheres.com/'
   browser.visit(url)
@@ -37,8 +48,42 @@ def scrape():
   html =  browser.html
   img_soup = BeautifulSoup(html, "html.parser")
 
-  # mars_dict["hemisphere_urls"]=
+  hemisphere_image_urls = []
+  img_item = img_soup.find_all('div', class_ = 'item')
+  img_item
 
-  browser.quit()
+  for item in img_item:
+    
+    div = item.find('div', class_='description')
+    href=div.a['href']
+    
+    hemi_url = (url + href)
+    
+    name = div.find('h3').text
+    
+    hem_name = name.rsplit(' ', 1)[0]
+     
+    browser.visit(hemi_url)
+        
+    html = browser.html
+        
+    img_soup = BeautifulSoup(html, 'html.parser')
+        
+    img_tag = img_soup.find('img', class_='wide-image')
+            
+    img = img_tag['src']
+        
+    img_url = (url + img)
+        
+    hemisphere_dict = {}
+    hemisphere_dict['title'] = hem_name
+    hemisphere_dict['img_url'] = img_url
+    hemisphere_image_urls.append(hemisphere_dict)
+  
+    mars_dict["img_url"]=img_url
 
-  return mars_dict
+    # mars_dict["hemisphere_dict"]=hemisphere_dict
+
+    browser.quit()
+
+    return mars_dict
